4314a7361a0a919cdb914d39529a9ca0d0d2ec43
alan <sky46821@gmail.com>
2021-12-15 11:30:11 +0800

    init

---
+++ _nuxt/304e606.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{281:function(e,n,t){"use strict";t.r(n);var c={layout:"admin",mounted:function(){this.$store.commit("changePage","basic-elements")}},l=t(22),component=Object(l.a)(c,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("basic-elements")])}),[],!1,null,null,null);n.default=component.exports}}]);
